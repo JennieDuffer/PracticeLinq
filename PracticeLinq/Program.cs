74ab2aef5eb8de6decac9a671bc47e0d09f22fbf
Jennie Duffer <jenniej297@gmail.com>
2022-08-26 11:06:19 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+List<string> videoGameNames = new List<string>
+{"Minecraft", "Call of Duty", "Grand Theft Auto", "Sim City"};
+
+IEnumerable<string> gameNames = videoGameNames.OrderBy(x => x.Length);
+
+foreach (string game in gameNames)
+{
+    Console.WriteLine(game);
+}
